d87d4bad22e15092a94c23ea62f6f8f686a782b8
zshelysze <angelebbah297@gmail.com>
2024-01-26 16:47:53 +0800

    hello mars

--- src/testproject/TestProject.java
+++ src/testproject/TestProject.java
@@ -15,6 +15,9 @@ public class TestProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello world");
+        System.out.println("hello mars");
+        
     }
     
 }
